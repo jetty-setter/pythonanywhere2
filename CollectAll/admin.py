ba7846b25cd8349d7c9070ba3d3e15040cf5595e
jetty-setter <123393626+jetty-setter@users.noreply.github.com>
2023-05-17 21:26:00 -0500

    Delete CollectAll directory

--- CollectAll/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Collection, CollectionItem, CollectionType, SiteUser, Comment, Category
-
-admin.site.register(Collection)
-admin.site.register(CollectionItem)
-admin.site.register(CollectionType)
-admin.site.register(SiteUser)
-admin.site.register(Comment)
-admin.site.register(Category)
--- CollectAll/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CollectallConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CollectAll'
